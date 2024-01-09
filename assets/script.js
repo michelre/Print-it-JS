64a621747d54bf573b1ff8601acfc35b6925d0c2
Rémi Michel <remi.michel38@gmail.com>
2024-01-09 19:03:12 +0100

    Session du 09/01

--- assets/script.js
+++ assets/script.js
@@ -14,5 +14,67 @@ const slides = [
 	{
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+	},
 ]
+
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const bannerImg = document.querySelector('.banner-img')
+const bannerParagraph = document.querySelector('#banner p')
+const dots = document.querySelector('.dots')
+const dotButtons = []
+let currentSlide = 0;
+
+function changeSlide(){
+	const currentImg = slides[currentSlide]['image']
+	const currentParagraph = slides[currentSlide]['tagLine']
+
+	//bannerImg.setAttribute('src', './assets/images/slideshow/' + currentImg)
+	bannerImg.setAttribute('src', `./assets/images/slideshow/${currentImg}`)
+	bannerParagraph.innerHTML = currentParagraph
+
+	dotButtons.forEach((dot, index) => {
+		dot.classList.remove('dot_selected')
+		if(index === currentSlide){
+			dot.classList.add('dot_selected')
+		}
+	})
+}
+
+arrowLeft.addEventListener('click', function(){
+	// Enlever 1 à currentSlide
+	if(currentSlide === 0){
+		currentSlide = slides.length - 1
+	} else {
+		currentSlide -= 1
+	}
+
+	changeSlide()
+})
+
+
+arrowRight.addEventListener('click', function(){
+	// Ajouter 1 à currentSlide
+	if(currentSlide === slides.length - 1){
+		currentSlide = 0
+	} else {
+		currentSlide += 1		
+	}
+
+	changeSlide()
+})
+
+slides.forEach((slide, index) => {
+	const dot = document.createElement('button')
+	dot.classList.add('dot')	
+
+	dot.addEventListener('click', () => {
+		currentSlide = index
+		changeSlide()
+	})
+
+	dots.appendChild(dot)
+	dotButtons.push(dot)
+})
+
+changeSlide()
